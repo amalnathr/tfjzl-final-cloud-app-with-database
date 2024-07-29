e8bf80d28895d7a60dcd90b0b2ea77b10dbd2edf
amalnath <amalnath.r@sparsh.io.io>
2024-07-29 20:06:05 +0530

    New changes

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -4,21 +4,29 @@ from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-    path(route='', view=views.CourseListView.as_view(), name='index'),
+    # Home page - course list view
+    path('', views.CourseListView.as_view(), name='index'),
+    
+    # User registration
     path('registration/', views.registration_request, name='registration'),
+    
+    # User login
     path('login/', views.login_request, name='login'),
+    
+    # User logout
     path('logout/', views.logout_request, name='logout'),
-    # ex: /onlinecourse/5/
+    
+    # Course details
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
+    
+    # Enroll in a course
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
-
-    # <HINT> Create a route for submit view
-
-    # <HINT> Create a route for show_exam_result view
-
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    # Submit exam answers
+    path('<int:course_id>/submit/', views.submit, name='submit'),
+    
+    # Show exam result
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='exam_result'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
